f14e285e15b9a8ee1c429fdd25b979fb674af90f
Alaina Hu <alaina.hu@mail.utoronto.ca>
2024-04-12 14:34:03 -0400

    Cleaned data.

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -9,50 +9,73 @@
 
 #### Workspace setup ####
 library(tidyverse)
+library(readxl)
 
 #### Clean data ####
-raw_data <- read_csv("data/raw_data/raw_data.csv")
-gdp_raw_data <- read_csv("data/raw_data/gdp_raw_data.csv")
-edu_raw_data <- read_csv("data/raw_data/edu_raw_data.csv")
-wagegap_raw_data <- read_csv("data/raw_data/wagegap_raw_data.csv")
+attitude_data <- read_csv("data/raw_data/raw_data.csv")
+
+gendergap_data <- read_excel("data/raw_data/global-gender-gap-index-2023.xlsx")
+
+gdp_data <- read_csv("data/raw_data/gdp_data.csv")
+
+
+
+
 
 attitude_data <-
-  raw_data |>
+  attitude_data |>
   select(Region, LOCATION, Country, Variables, Value) |>
   filter(Region == "All regions") |> 
-  filter(Variables == "Attitudes on women's income") |>
+  filter(Variables %in%  c("Attitudes on women's income", "Attitudes justifying intimate-partner violence", "Attitudes on women's ability to be a political leader")) |>
   mutate(Value = as.numeric(Value)) |>
   select(-Region, -Variables) |>
   rename(attitude = Value) 
 
+attitude_data <- attitude_data |>
+  group_by(Country) |>
+  mutate(attitude_id = row_number()) |>
+  ungroup()
+
+attitude_data <- attitude_data |>
+  pivot_wider(
+    names_from = attitude_id,
+    values_from = attitude,
+    names_prefix = "attitude_"
+    )
+
+
+attitude_data <- attitude_data |>
+  drop_na(wide_data) |>
+  rename(
+    `Income Attitude` = attitude_1,
+    `Violence Attitude` = attitude_2,
+    `Political Attitude` = attitude_3
+  )
+
+
+column_names <- names(gendergap_data)
+column_names[1] <- "Country"
+column_names[2] <- "Gender Gap"
+names(gendergap_data) <- column_names
+
+
+column_names_1 <- names(gdp_data)
+column_names_1 <- c("Series", "Series Code", "Country", "Code", "GDP")
+names(gdp_data) <- column_names_1
+
 gdp_data <-
-  gdp_raw_data |>
-  select(LOCATION, Country, Year, Value) |>
-  filter(Year == "2022") |>
-  mutate(Value = as.numeric(Value)) |>
-  rename(gdp = Value) |>
-  select(-Year)
+  gdp_data |>
+  select(Country, Code, GDP)
 
-edu_data <- 
-  edu_raw_data |>
-  select(LOCATION, TIME, Value) |>
-  filter(TIME == "2022") |>
-  mutate(Value = as.numeric(Value)) |>
-  rename(education = Value) |>
-  select(-TIME)
-  
-wagegap_data <- 
-  wagegap_raw_data |> 
-  select(LOCATION, SUBJECT, TIME, Value) |>
-  filter(TIME == "2020") |>
-  filter(SUBJECT == "EMPLOYEE") |>
-  mutate(Value = as.numeric(Value)) |>
-  rename(wagegap = Value) |>
-  select(-TIME, -SUBJECT)
 
-merged_data <- left_join(attitude_data, gdp_data)  
-merged_data <- left_join(merged_data, edu_data)
-merged_data <- left_join(merged_data, wagegap_data)
-merged_data <- na.omit(merged_data)
+merged_data <- left_join(attitude_data, gdp_data, by = "Country")  
+merged_data <- left_join(merged_data, gendergap_data, by = "Country")
+merged_data <- merged_data |>
+  select(-LOCATION, -Code)
+
+
+
+
+
 #### Save data ####
-write_csv(cleaned_data, "outputs/data/analysis_data.csv")
+write_csv(cleaned_data, "data/analysis_data.csv")
