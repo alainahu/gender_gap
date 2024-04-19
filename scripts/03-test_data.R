55fbeaab925a0a7c3f6c08d6abfd66864394225a
Alaina Hu <alaina.hu@mail.utoronto.ca>
2024-04-19 02:59:34 -0500

    Added datasheet.

--- scripts/03-test_data.R
+++ scripts/03-test_data.R
@@ -1,15 +1,54 @@
 #### Preamble ####
-# Purpose: Tests... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Tests the real dataset
+# Author: Alaina Hu
+# Date: 18 April 2024
+# Contact: alaina.hu@utoronto.ca 
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: Have access to the analysis dataset
+# Any other information needed? NA
 
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
+library(countrycode)
 
 #### Test data ####
+analysis_data <- read_csv("data/analysis_data/analysis_data.csv")
+country_names <- countrycode::codelist$country.name.en
+
+analysis_data$Country |>
+  unique() %in% country_names
+
+analysis_data$`Income Attitude` |>
+  min() >= 0
+
+analysis_data$`Income Attitude` |>
+  max() <= 100
+
+analysis_data$`Violence Attitude` |>
+  min() >= 0
+
+analysis_data$`Violence Attitude` |>
+  max <= 100
+
+analysis_data$`Political Attitude` |>
+  min() >= 0
+
+analysis_data$`Political Attitude` |>
+  max <= 100
+
+analysis_data$GDP |>
+  min() >= 0
+
+analysis_data$GDP |>
+  max() <= 200000
+
+analysis_data$`Gender Gap` |>
+  min() >= 0
+
+analysis_data$`Gender Gap` |>
+  max() <= 1
+
+
+
+
